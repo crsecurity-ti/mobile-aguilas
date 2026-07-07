e7a8821e1fb0438c237939dae5c65864bfd550bc
Daniel Cares <consultoria.karpio@gmail.com>
2026-07-07 17:14:58 -0400

    fix firebase sdk version pin para resolver grpc-core en ios

--- plugins/withGrpcFix.js
+++ plugins/withGrpcFix.js
@@ -2,6 +2,8 @@ const { withDangerousMod } = require("@expo/config-plugins");
 const fs = require("fs");
 const path = require("path");
 
+// FirebaseFirestoreInternal 11.8.0 requiere gRPC-Core ~> 1.65.0 (solo pre-release en CocoaPods).
+// Pinear Firebase iOS SDK a 11.6.0 que usa gRPC-Core ~> 1.64.x (estable).
 module.exports = function withGrpcFix(config) {
   return withDangerousMod(config, [
     "ios",
@@ -9,13 +11,9 @@ module.exports = function withGrpcFix(config) {
       const podfilePath = path.join(mod.modRequest.platformProjectRoot, "Podfile");
       let contents = fs.readFileSync(podfilePath, "utf8");
 
-      const marker = "# gRPC-Core pre-release fix";
+      const marker = "# Firebase iOS SDK version pin - gRPC-Core fix";
       if (!contents.includes(marker)) {
-        // Insert before post_install block (still inside target block)
-        contents = contents.replace(
-          /(\n\s*post_install do \|installer\|)/,
-          `\n  ${marker}\n  pod 'gRPC-Core', '1.65.5'\n$1`
-        );
+        contents = `${marker}\n$FirebaseSDKVersion = '11.6.0'\n\n` + contents;
         fs.writeFileSync(podfilePath, contents);
       }
 
